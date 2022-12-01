7dee01352a606e2452a8260f8b3868bd5deb5328
Omid.Haddi@gmail.com <omidhaddi@Omids-MacBook-Pro.local>
2022-12-01 12:27:18 +0100

    some line has been deleted

--- pages/flats/new.js
+++ pages/flats/new.js
@@ -30,15 +30,7 @@ export default function NewFlat(currentUser) {
                     <input className="btn btn-primary btn-lg" type="submit" value="Submit" />
                 </form>
             </div>
-            <button className="f19g2zq0 dir dir-ltr" type='button'>
-                <div className="f1xx50dm dir dir-ltr" id="littleSearchLabel">Start Your Search</div>
-                <div className="s1qcpybl dir dir-ltr" data-icon="true" data-testid="little-search-icon">
-                    <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{display: 'block', fill: 'none', height: 12, width: 12, stroke: 'currentcolor', strokeWidth: 5.33333, overflow: 'visible'}}>
-                        <g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path>
-                        </g>
-                    </svg>
-                </div>
-            </button>
+
         </>
     )
 }

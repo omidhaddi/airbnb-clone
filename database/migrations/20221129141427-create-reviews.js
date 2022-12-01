a6102bbc70770846e3a1e333a760104005d55006
Mutahari562 <nur.mutahari@gmail.com>
2022-12-01 12:09:43 +0100

    rooms and users controller

--- database/migrations/20221129141427-create-reviews.js
+++ database/migrations/20221129141427-create-reviews.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('reviews', {
+    await queryInterface.createTable('Reviews', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -29,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('reviews');
+    await queryInterface.dropTable('Reviews');
   }
 };

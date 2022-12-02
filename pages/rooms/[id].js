6b94c3fa94e072c0d24584397ce1ecd610588968
Mutahari562 <nur.mutahari@gmail.com>
2022-12-02 09:58:41 +0100

    added booking

--- pages/rooms/[id].js
+++ pages/rooms/[id].js
@@ -54,3 +54,55 @@ export async function getServerSideProps(req, res) {
 }
 
 // 3 guests · 1 bedroom · 2 beds · 1 bath
+const bookingsController = {
+    create: async (data) => {
+      console.log(data)
+      const startBooking = new Date(data.startDate);
+      const endBooking = new Date(data.endDate);
+      /* avoid booking overlapping
+      1. booking starting in other booking
+        startDate <= startBooking &&  endDate >= startBooking
+      2. booking ending in other booking
+        startDate <= endBooking &&  endDate >= endBooking
+      3. booking range including pre-existing one
+        startDate >= startBooking && endDate <= endBooking 
+      */
+      const existingBooking = await db.Booking.findOne({
+        where: {
+          [Op.and]: [
+            { spaceshipId: data.spaceshipId },
+            {
+              [Op.or]: [
+                {
+                  [Op.and]: [
+                    { startDate: { [Op.lte]: startBooking } },
+                    { endDate: { [Op.gte]: startBooking } }
+                  ]
+                },
+                {
+                  [Op.and]: [
+                    { startDate: { [Op.lte]: endBooking } },
+                    { endDate: { [Op.gte]: endBooking } },
+                  ]
+                },
+                {
+                  [Op.and]: [
+                    { startDate: {[Op.gte]: startBooking } },
+                    { endDate: {[Op.lte]: endBooking } }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      })
+  
+      console.log(existingBooking)
+      
+      if (existingBooking) {
+        return "error"
+      }
+      
+      return await db.Booking.create(data)
+    }
+  }

6c04e3821b5892db22efbf9f8de44fc620f70e21
OMID HADDI <113886529+omidhaddi@users.noreply.github.com>
2022-12-05 13:22:54 +0100

    Update config.mjs

--- database/config/config.mjs
+++ database/config/config.mjs
@@ -4,13 +4,10 @@ dotenv.config();
 
 const config = {
   "development": {
-<<<<<<< HEAD
-    "username": "avinash",
+    "username": "omidhaddi",
     "password": "password",
-=======
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
->>>>>>> 927eaa87234fb2343897d5433d368358b8bed710
     "database": "airbnb-clone",
     "host": "127.0.0.1",
     "dialect": "postgres"
